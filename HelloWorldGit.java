d9a4fa3cc2e3e95e5eb3a79f9701bfc303ae0b4c
asim shaikh <asim_shaikh@persistent.com>
2018-07-26 15:39:24 +0530

    added date()

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,5 +1,7 @@
 class HellowWorld{
 
+	static void disply(){
+		system.out.println("Display today date"new Date());
 
 	public static void main(String []args){
 
@@ -7,7 +9,7 @@ class HellowWorld{
 		systom.out.println("Hello World\n");
 
 		HelloWorld obj=new HelloWorld();
-
+		obj.display();
 
 	}
 
